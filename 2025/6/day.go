74a995fb24efcf3436cdad7d518d62c294422351
Jorge Bernal <jorge@automattic.com>
2025-12-06 23:04:11 +0100

    2025 day 6 part 1

---
+++ 2025/6/day.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"aoc/utils"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Running 2025 Day 6")
+
+	inputPath := utils.InitConfig().InputPath
+	utils.RunPart("Part 1", inputPath, part1)
+	utils.RunPart("Part 2", inputPath, part2)
+}
+
+type operation string
+
+const (
+	addition       operation = "+"
+	multiplication operation = "*"
+)
+
+type problem struct {
+	values   []int
+	operator operation
+}
+
+func parseInput(input string) []problem {
+	lines := utils.ReadLines(input)
+	problems := make([]problem, len(strings.Fields(lines[0])))
+	for _, line := range lines {
+		values := strings.Fields(line)
+		for i, v := range values {
+			number, err := strconv.Atoi(v)
+			if err != nil {
+				problems[i].operator = operation(v)
+			} else {
+				problems[i].values = append(problems[i].values, number)
+			}
+		}
+	}
+	return problems
+}
+
+func solve(prob problem) int {
+	solution := prob.values[0]
+	for _, v := range prob.values[1:] {
+		switch prob.operator {
+		case addition:
+			solution += v
+		case multiplication:
+			solution *= v
+		default:
+			panic("unknown operator")
+		}
+	}
+	return solution
+}
+
+func part1(input string) int {
+	problems := parseInput(input)
+
+	solution := 0
+	for _, prob := range problems {
+		solution += solve(prob)
+	}
+	return solution
+}
+
+func part2(input string) int {
+	return 0
+}
